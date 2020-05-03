e8a5bc7fb9d9b83e7ea57a580ef7602c296d6951
TheNomanAhmed <nomanahmed478@gmail.com>
2020-05-03 19:25:08 +0300

    gatsby mdx plugin and sharp plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,21 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
   ],
 }
