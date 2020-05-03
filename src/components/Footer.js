91ec53759ea5a43e65b0c2f8caac4ebe953f602a
TheNomanAhmed <nomanahmed478@gmail.com>
2020-05-03 22:49:54 +0300

    Added Footer and some styling

---
+++ src/components/Footer.js
@@ -0,0 +1,64 @@
+import React from "react"
+import {
+  FooterWrapper,
+  FooterSocialWrapper,
+  FooterSocialIcons,
+} from "../elements"
+import { useStaticQuery, graphql } from "gatsby"
+
+export const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      facebook: file(relativePath: { eq: "facebook.svg" }) {
+        publicURL
+      }
+      github: file(relativePath: { eq: "github.svg" }) {
+        publicURL
+      }
+      styled: file(relativePath: { eq: "styled.svg" }) {
+        publicURL
+      }
+      gatsby: file(relativePath: { eq: "gatsby.svg" }) {
+        publicURL
+      }
+    }
+  `)
+
+  return (
+    <FooterWrapper>
+      <FooterSocialWrapper>
+        <FooterSocialIcons>
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.facebook.publicURL} alt="facebook Logo" />
+          </a>
+          <a
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.github.publicURL} alt="github Logo" />
+          </a>
+          <a
+            href="https://gatsby.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.gatsby.publicURL} alt="gatsby Logo" />
+          </a>
+          <a
+            href="https://styled.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={data.styled.publicURL} alt="Styled Logo" />
+          </a>
+        </FooterSocialIcons>
+        <p> 👋 Copyrights Noman Ahmed Khan </p>
+      </FooterSocialWrapper>
+    </FooterWrapper>
+  )
+}
