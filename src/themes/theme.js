70a59eb1e3f1479e2590023944375435ddd751a4
TheNomanAhmed <nomanahmed478@gmail.com>
2020-05-03 20:28:04 +0300

    Added Theme.js and styled components

---
+++ src/themes/theme.js
@@ -0,0 +1,37 @@
+export default {
+  fonts: {
+    main: "Muli, sans.serif",
+    code: "Roboto Mono, monospace",
+  },
+  colors: {
+    main1: "#21243d",
+    main2: "#ff7c7c",
+    dark1: "#1b262c",
+    dark2: "#222831",
+    dark3: "#363062",
+    light1: "#d4ebd0",
+    light2: "#f1e6d1",
+  },
+  breakpoints: {
+    mobile: "only screen and (max-width: 50rem)",
+    tablet: "only screen and (max-width: 65rem)",
+  },
+  spacing: {
+    xxSmall: ".25rem",
+    xSmall: ".5rem",
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+  },
+  animations: {
+    button: "box-shadow 0.3s ease",
+    link: "color 0.2s ease",
+  },
+  shadows: {
+    smShadow: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
+    mdShadow:
+      "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+    lgshadow:
+      "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+  },
+}
